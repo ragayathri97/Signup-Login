c960536c38169136d094a7f3ad277ceacd900c36
ragayathri97 <ragayathri97@gmail.com>
2024-12-05 20:29:44 +0530

    feature/signup

---
+++ index.js
@@ -0,0 +1,13 @@
+let userNames=["Alice","Bob","charlie","sri","ram"];
+function signup(str){
+    let filteredData=userNames.filter((el,i)=>el==str);
+   
+    if(filteredData.length==0){
+        userNames.push(str)
+        console.log("signup Sucessfull......")
+    }
+    else{
+        console.log("Already registred....")
+    }
+}
+signup("Bob")
